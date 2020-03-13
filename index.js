d15cea24a71ec56b147996a7cabe68094afd3f54
Raul Gomez <raul_nouni@hotmail.com>
2020-03-12 23:34:17 -0600

    Custom Types and add standar linter

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path') 
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
 // Definiendo el Schema
-const schema = buildSchema(`
-  type Query{
-    hello: String
-    saludo: String
-  }
-`)
-
-// Configurar los resolvers
-const resolvers = {
-  hello: () => {
-    return 'Hello World'
-  },
-  saludo: () => {
-    return 'Hola buenas noches'
-  }
-}
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
 // Ejecutar el query hello en consola
 // graphql(schema, '{ saludo }', resolvers).then((data) => {
